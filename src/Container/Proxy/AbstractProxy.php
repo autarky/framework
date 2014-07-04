e0fc31189fe11ae282b14c83def634623e01f3ec
Andreas Lutro <anlutro@gmail.com>
2014-07-04 06:53:51 +0200

    implement container proxies

---
+++ src/Container/Proxy/AbstractProxy.php
@@ -0,0 +1,43 @@
+<?php
+namespace Autarky\Container\Proxy;
+
+use Autarky\Container\ContainerInterface;
+
+abstract class AbstractProxy
+{
+	protected static $instances = [];
+	protected static $container;
+
+	public static function setProxyContainer(ContainerInterface $container = null)
+	{
+		static::$instances = [];
+		static::$container = $container;
+	}
+
+	public static function setProxyInstance($instance = null)
+	{
+		static::$instances[static::getProxyIocKey()] = $instance;
+	}
+
+	protected static function resolveProxyInstance()
+	{
+		return static::$container->resolve(static::getProxyIocKey());
+	}
+
+	public static function __callStatic($method, array $args)
+	{
+		$key = static::getProxyIocKey();
+
+		if (!isset(static::$instances[$key])) {
+			static::$instances[$key] = static::resolveProxyInstance();
+		}
+
+		return call_user_func_array([static::$instances[$key], $method], $args);
+	}
+
+	protected static function getProxyIocKey()
+	{
+		// abstract static methods are not allowed, so do this instead
+		throw new \RuntimeException('Method getProxyIocKey must be implemented.');
+	}
+}
---
+++ tests/Container/ProxyTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Autarky\Tests\Container;
+
+use Mockery as m;
+
+use Autarky\Tests\TestCase;
+use Autarky\Container\Container;
+use Autarky\Container\Proxy\AbstractProxy;
+
+class ProxyTest extends TestCase
+{
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	/** @test */
+	public function proxyIsResolved()
+	{
+		AbstractProxy::setProxyContainer($container = new Container);
+		$container->share('foo', $mock1 = m::mock());
+		$container->share('baz', $mock2 = m::mock());
+		$mock1->shouldReceive('foo')->once()->with('bar')->andReturn('baz');
+		$mock2->shouldReceive('baz')->once()->with('bar')->andReturn('foo');
+		$this->assertEquals('baz', StubProxy::foo('bar'));
+		$this->assertEquals('foo', StubProxyTwo::baz('bar'));
+	}
+}
+
+class StubProxy extends AbstractProxy
+{
+	protected static function getProxyIocKey()
+	{
+		return 'foo';
+	}
+}
+class StubProxyTwo extends AbstractProxy
+{
+	protected static function getProxyIocKey()
+	{
+		return 'baz';
+	}
+}
