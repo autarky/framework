b90d2fc87c35bf9015ce02e6138fba43acb935ba
Andreas Lutro <anlutro@gmail.com>
2014-09-13 11:52:23 +0200

    change app to kernel and general tidying up

--- src/Session/Middleware.php
+++ src/Session/Middleware.php
@@ -22,25 +22,49 @@ use Autarky\Kernel\Application;
  */
 class Middleware implements HttpKernelInterface
 {
+	/**
+	 * @var HttpKernelInterface
+	 */
 	protected $kernel;
-	protected $app;
+
+	/**
+	 * @var SessionInterface
+	 */
 	protected $session;
+
+	/**
+	 * Whether the sessions should always be started. Normal behaviour is to
+	 * start the sesion whenever an operation is requested from it.
+	 *
+	 * @var bool
+	 */
 	protected $forceStart;
+
+	/**
+	 * Additional cookie parameters to add to the session ID cookie.
+	 *
+	 * @var array
+	 */
+	protected $cookies;
 
 	public function __construct(HttpKernelInterface $kernel, Application $app)
 	{
 		$this->kernel = $kernel;
-		$this->app = $app;
 		$this->session = $app->getContainer()
 			->resolve('Symfony\Component\HttpFoundation\Session\SessionInterface');
 		$this->forceStart = $app->getConfig()
 			->get('session.force', false);
+		$this->cookies = $app->getConfig()
+			->get('session.cookies', []);
 	}
 
+	/**
+	 * {@inheritdoc}
+	 */
 	public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
 	{
 		if ($type !== HttpKernelInterface::MASTER_REQUEST) {
-			return $this->app->handle($request, $type, $catch);
+			return $this->kernel->handle($request, $type, $catch);
 		}
 
 		$request->setSession($this->session);
@@ -62,10 +86,7 @@ class Middleware implements HttpKernelInterface
 		if ($this->session->isStarted()) {
 			$this->session->save();
 
-			$params = array_merge(
-				session_get_cookie_params(),
-				$this->app->getConfig()->get('session.cookie', [])
-			);
+			$params = array_merge(session_get_cookie_params(), $this->cookies);
 
 			if ($params['lifetime'] !== 0) {
 				$params['lifetime'] = $request->server->get('REQUEST_TIME') + $params['lifetime'];
