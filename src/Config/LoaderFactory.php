4eb27d1ab8cc21cb33759ca6dc2f9d699303355f
Andreas Lutro <anlutro@gmail.com>
2014-09-17 17:56:04 +0200

    add missing file docblocks

--- src/Config/LoaderFactory.php
+++ src/Config/LoaderFactory.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the Autarky package.
+ *
+ * (c) Andreas Lutro <anlutro@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Autarky\Config;
 
 use Autarky\Container\ContainerInterface;
