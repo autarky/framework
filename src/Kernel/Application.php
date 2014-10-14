0093a0972682c114df6c57320aa0d1fb745d9b22
Andreas Lutro <anlutro@gmail.com>
2014-10-14 20:48:39 +0200

    Version 0.6.0

--- src/Kernel/Application.php
+++ src/Kernel/Application.php
@@ -34,7 +34,7 @@ class Application implements HttpKernelInterface
 	/**
 	 * The framework version.
 	 */
-	const VERSION = '0.6.x';
+	const VERSION = '0.6.0';
 
 	/**
 	 * The application's service providers.
