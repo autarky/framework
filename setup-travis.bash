cb1b701d7d750bc38d1eb775a6ee876b8158cb8a
Andreas Lutro <anlutro@gmail.com>
2014-10-15 22:33:03 +0200

    wrap in sh -c

--- setup-travis.bash
+++ setup-travis.bash
@@ -5,7 +5,8 @@
 PHP_VER=$(phpenv version-name)
 
 if [[ $PHP_VER == "hhvm" ]]; then
-	sudo echo "extension = mongo.so" >> /etc/hhvm/php.ini
+	sudo sh -c 'echo "extension = mongo.so" >> /etc/hhvm/php.ini'
+	# memcache and memcached are enabled by default
 else
 	echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 	echo "extension = memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
