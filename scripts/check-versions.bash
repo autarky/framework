bada99cab0bb86cc2107a6e0f5f987c845086ee1
Andreas Lutro <anlutro@gmail.com>
2015-02-22 22:44:47 +0100

    improve script

--- scripts/check-versions.bash
+++ scripts/check-versions.bash
@@ -2,11 +2,13 @@
 
 git_branch=$(git branch | sed -n '/\* /s///p')
 
-git_tag=$(git describe --abbrev=0)
-
-if [[ $git_tag = "" ]]; then
+# only master branch + minor version branches should be checked
+if [[ $git_branch != "master" && ! $git_branch =~ [0-9].[0-9] ]]; then
 	exit 0
 fi
+
+# gets the latest tag belonging to the current line of commits
+git_tag=$(git describe --abbrev=0)
 
 app_version=$(grep 'const VERSION' classes/Application.php | grep -E -o "[0-9\.]+")
 
