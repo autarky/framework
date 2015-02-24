3973e8ad24a19e85c008762264881de27ba2de0c
Andreas Lutro <anlutro@gmail.com>
2015-02-24 18:38:38 +0100

    improve httpkernel test

--- tests/unit/Kernel/HttpKernelTest.php
+++ tests/unit/Kernel/HttpKernelTest.php
@@ -5,6 +5,7 @@ use Mockery as m;
 use Autarky\Kernel\HttpKernel;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class HttpKernelTest extends PHPUnit_Framework_TestCase
 {
@@ -17,6 +18,7 @@ class HttpKernelTest extends PHPUnit_Framework_TestCase
 	{
 		$this->router = m::mock('Autarky\Routing\RouterInterface');
 		$this->errorHandler = m::mock('Autarky\Errors\ErrorHandlerInterface');
+		$this->errorHandler->shouldReceive('handle')->andReturnUsing(function($e) { throw $e; })->byDefault();
 		$this->requests = new \Symfony\Component\HttpFoundation\RequestStack;
 		$this->eventDispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher;
 		return new HttpKernel($this->router, $this->requests, $this->errorHandler, $events ? $this->eventDispatcher : null);
@@ -34,7 +36,7 @@ class HttpKernelTest extends PHPUnit_Framework_TestCase
 	public function requestListenerCanReturnResponse()
 	{
 		$kernel = $this->makeKernel();
-		$this->eventDispatcher->addListener('kernel.request', function($event) {
+		$this->eventDispatcher->addListener(KernelEvents::REQUEST, function($event) {
 			$event->setResponse(new Response('event response'));
 		});
 		$response = $kernel->handle(Request::create('/'));
@@ -46,7 +48,7 @@ class HttpKernelTest extends PHPUnit_Framework_TestCase
 	{
 		$kernel = $this->makeKernel();
 		$this->router->shouldReceive('dispatch')->once()->andReturn(new Response('router response'));
-		$this->eventDispatcher->addListener('kernel.response', function($event) {
+		$this->eventDispatcher->addListener(KernelEvents::RESPONSE, function($event) {
 			$event->setResponse(new Response('event response'));
 		});
 		$response = $kernel->handle(Request::create('/'));
@@ -78,7 +80,7 @@ class HttpKernelTest extends PHPUnit_Framework_TestCase
 		$kernel = $this->makeKernel();
 		$this->router->shouldReceive('dispatch')->once()->andThrow($e = new \Exception('router exception'));
 		$this->errorHandler->shouldReceive('handle')->once()->with($e)->andReturn(new Response('error handler response'));
-		$this->eventDispatcher->addListener('kernel.response', function() { throw new \Exception('listener exception'); });
+		$this->eventDispatcher->addListener(KernelEvents::RESPONSE, function() { throw new \Exception('listener exception'); });
 		$response = $kernel->handle(Request::create('/'));
 		$this->assertEquals('error handler response', $response->getContent());
 	}
@@ -88,7 +90,7 @@ class HttpKernelTest extends PHPUnit_Framework_TestCase
 	{
 		$kernel = $this->makeKernel();
 		$this->router->shouldReceive('dispatch')->once()->andThrow($e = new \Exception('router exception'));
-		$this->eventDispatcher->addListener('kernel.exception', function($event) {
+		$this->eventDispatcher->addListener(KernelEvents::EXCEPTION, function($event) {
 			$event->setResponse(new Response('exception listener response'));
 		});
 		$response = $kernel->handle(Request::create('/'));
@@ -100,10 +102,72 @@ class HttpKernelTest extends PHPUnit_Framework_TestCase
 	{
 		$kernel = $this->makeKernel();
 		$triggered = false;
-		$this->eventDispatcher->addListener('kernel.terminate', function() use(&$triggered) {
+		$this->eventDispatcher->addListener(KernelEvents::TERMINATE, function() use(&$triggered) {
 			$triggered = true;
 		});
 		$kernel->terminate(Request::create('/'), new Response(''));
 		$this->assertTrue($triggered);
+	}
+
+	/** @test */
+	public function requestStackIsCorrect()
+	{
+		$kernel = $this->makeKernel();
+		$triggered = false;
+		$this->router->shouldReceive('dispatch')->once()
+			->andReturnUsing(function() use(&$triggered) {
+				$triggered = true;
+				$this->assertEquals(1, count($this->requests));
+				return new Response('foo');
+			});
+		$response = $kernel->handle($request = Request::create('/'));
+		$kernel->terminate($request, $response);
+	}
+
+	/** @test */
+	public function nestedRequestStackIsCorrect()
+	{
+		$kernel = $this->makeKernel();
+		$counter = 0;
+		$first = function($request) use(&$counter, $kernel) {
+			$asserts = function() use($request) {
+				$this->assertSame(
+					$request,
+					$this->requests->getCurrentRequest()
+				);
+				$this->assertSame(
+					$this->requests->getCurrentRequest(),
+					$this->requests->getMasterRequest()
+				);
+				$this->assertNull($this->requests->getParentRequest());
+				$this->assertEquals(1, count(static::readAttribute($this->requests, 'requests')));
+			};
+			$counter++;
+			$asserts();
+			$innerRequest = clone $request;
+			$result = $kernel->handle($innerRequest);
+			$asserts();
+			return $result;
+		};
+		$second = function($request) use(&$counter) {
+			$counter++;
+			$this->assertNotNull($this->requests->getParentRequest());
+			$this->assertSame(
+				$this->requests->getMasterRequest(),
+				$this->requests->getParentRequest()
+			);
+			$this->assertNotSame(
+				$this->requests->getCurrentRequest(),
+				$this->requests->getMasterRequest()
+			);
+			$this->assertEquals(2, count(static::readAttribute($this->requests, 'requests')));
+			return new Response('foo');
+		};
+		$this->router->shouldReceive('dispatch')->twice()
+			->andReturnUsing($first, $second);
+		$response = $kernel->handle($request = Request::create('/'));
+		$kernel->terminate($request, $response);
+		$this->assertEquals(2, $counter);
+		$this->assertEquals('foo', $response->getContent());
 	}
 }
