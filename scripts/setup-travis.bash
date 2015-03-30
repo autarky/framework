2be2eef4125602533d4a6bdb05807aa801b74247
Andreas Lutro <anlutro@gmail.com>
2015-03-30 21:32:08 +0200

    check if travis version contains hhvm instead of equals

--- scripts/setup-travis.bash
+++ scripts/setup-travis.bash
@@ -2,7 +2,7 @@
 
 set -ev
 
-if [ "$TRAVIS_PHP_VERSION" != "hhvm" ]; then
+if [ "$TRAVIS_PHP_VERSION" == *"hhvm"* ]; then
 	INI_PATH=~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 	echo "extension = mongo.so" >> $INI_PATH
 	echo "extension = memcache.so" >> $INI_PATH
