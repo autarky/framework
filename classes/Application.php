1473c44a70df68b2b502f705291ebfc6c84c52b5
Andreas Lutro <anlutro@gmail.com>
2016-05-20 20:29:52 +0200

    version 0.10.1

--- classes/Application.php
+++ classes/Application.php
@@ -37,7 +37,7 @@ class Application implements HttpKernelInterface
 	/**
 	 * The framework version.
 	 */
-	const VERSION = '0.10.0';
+	const VERSION = '0.10.1';
 
 	/**
 	 * The application's service providers.
